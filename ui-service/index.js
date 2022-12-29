0547594a60db54c28d62fa516abbf9f3cfd8e38d
Shahar Solomianik <shahar@gnift.com>
2022-12-29 13:01:01 +0200

    initial

---
+++ ui-service/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const tasksRouter = require('./routes/tasks');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// config
+const PORT = process.env.PORT || 3000;
+const HOST_NAME = process.env.HOST_NAME || 'localhost'
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// route setup
+app.use('/tasks', tasksRouter);
+app.use('/users', usersRouter);
+
+// db setup
+// const db = require('./models');
+// db.sequelize.sync();
+
+// start
+app.listen(PORT, HOST_NAME || 'localhost', ()=> {
+    console.log(`Server running at ${HOST_NAME}:${PORT}`)
+})
