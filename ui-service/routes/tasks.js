2306797ff1f0303eabf9945a3506187dbad69754
Shahar Solomianik <shahar@gnift.com>
2022-12-31 19:01:09 +0200

    alota

--- ui-service/routes/tasks.js
+++ ui-service/routes/tasks.js
@@ -6,6 +6,22 @@ const healthcheck = async (req, res) => {
     res.send('I\'m healthy');
 }
 
+const userTasks = async (req, res) => {
+    try {
+        const user = axios.post(`http://localhost:3002/user-tasks/${req.params.user_id}`,{
+            data: {
+                username: req.body.username,
+                password: req.body.password,
+            }
+        });
+        res.redirect(`/tasks/user/${user.id}`)
+
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
 router.get('/healthcheck/', healthcheck);
+router.get('/user/:user_id', userTasks);
 
 module.exports = router;
--- ui-service/routes/users.js
+++ ui-service/routes/users.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
-
+const axios = require('axios');
 
 const healthcheck = async (req, res) => {
     res.send('I\'m healthy');
 }
 
-router.get('/healthcheck/', healthcheck);
+const signInPage = async (req, res) => {
+    res.render('users/sign-in');
+}
+const signIn = async (req, res) => {
+    try {
+        const user = axios.post('http://localhost:3001/sign-in',{
+            data: {
+                username: req.body.username,
+                password: req.body.password,
+            }
+        });
+        res.redirect(`/tasks/user/${user.id}`)
+
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+router.get('/healthcheck', healthcheck);
+router.get('/sign-in', signInPage);
+router.post('/sign-in', signIn);
 
 module.exports = router;
