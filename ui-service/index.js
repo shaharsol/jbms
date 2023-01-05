699b333ed2ba3b21f535781aa6aa8363e8915f2f
Shahar Solomianik <shahar@gnift.com>
2023-01-05 12:59:19 +0200

    removed comments

--- ui-service/index.js
+++ ui-service/index.js
@@ -12,19 +12,13 @@ const HOST_NAME = process.env.HOST_NAME || 'localhost'
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // route setup
 app.use('/tasks', tasksRouter);
 app.use('/users', usersRouter);
-
-// db setup
-// const db = require('./models');
-// db.sequelize.sync();
 
 // start
 app.listen(PORT, HOST_NAME || 'localhost', ()=> {
